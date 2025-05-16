5d7f7eeb4844fee63e107551ae670c9c7c444f51
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-16 18:16:48 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Description: '''
+
+
+# Name: lab2f.py
+# Author: Chung Yin Choi
+# Author ID: cychoi
+# Date Created: 2025/05/16
+
+import sys
+
+count = int(sys.argv[1])
+
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
+
+
+
