9e54dbbb5899bb0e6ba5a14fd72d07a0a9fbb912
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-16 18:14:52 -0400

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Description: This program will output "Hi, you are years old." to the screen.'''
+
+
+# Name   : lab2b.py
+# Author : Chung Yin Choi
+# Date   : May 15, 2025
+
+name = input("Name: ")
+age = input("Age: ")
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
