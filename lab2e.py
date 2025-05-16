1d7bd39250b38c5fbeec55b5c0d276de9c8f7f7b
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-16 18:16:38 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Description: '''
+
+
+# Name: lab2e.py
+# Author: Chung Yin Choi
+# Author ID: cychoi
+# Date Created: 2025/05/16
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
+
+
