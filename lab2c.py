225b126e9152da331b5124778469c7c55ac2f1b1
Chung Yin Choi <cychoi@myseneca.ca>
2025-05-16 18:16:20 -0400

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Description: Using Arguments'''
+
+
+# Name   : lab2c.py
+# Author : Chung Yin Choi
+# Date   : May 15, 2025
+
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
